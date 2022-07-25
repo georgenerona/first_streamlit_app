cca6d3610ebdf25df8cac3d92febdc5c4f7e61dd
George Nerona <109990380+georgenerona@users.noreply.github.com>
2022-07-25 15:02:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega  & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled free-Range Egg')
